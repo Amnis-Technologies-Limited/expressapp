9911cf762b8cba5dff4d21899393213d07ef5a8a
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2023-07-18 10:50:53 +0100

    server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParset = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+let corsOptions = {
+    origin: 'http://localhost:8081'
+}
+
+app.use(cors(corsOptions))
+
+// Parse request of content-type application/json
+app.use(express.json())
+
+// Parse request of content-type application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// Simple route
+app.get("/", (req, res) => {
+    res.json({
+        message: 'Welcome to Amnis Backend with Node Express & Postgresql'
+    })
+})
+
+// Set port
+const PORT = 8080;
+app.listen(PORT,  () => {
+    console.log('Server is running on port: ' + PORT)
+})
