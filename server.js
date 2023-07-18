e46ddfafee18c3c5485adade9efe32fd8801f7bf
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2023-07-18 11:27:12 +0100

    DB added

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ let corsOptions = {
     origin: 'http://localhost:8081'
 }
 
+const db = require('./models')
+db.sequelize.sync()
+    .then(() => {
+        console.log('Synced with DB')
+    })
+    .catch((err) => {
+        console.log('Error connecting to Db: ' + err.message)
+    })
+
 app.use(cors(corsOptions))
 
 // Parse request of content-type application/json
@@ -25,6 +34,6 @@ app.get("/", (req, res) => {
 
 // Set port
 const PORT = 8080;
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
     console.log('Server is running on port: ' + PORT)
 })
