8198a5c36b7868fcd89febdadc429ab13274af7b
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2023-07-25 11:53:30 +0100

    Added Login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,7 +24,7 @@ const createUser = async (req, res) => {
                 expiresIn: 1 * 24 * 60 * 60 * 1000
             })
 
-            res.cookie("jwt", token, {maxAge:  1 * 24 * 60 * 60, httpOnly: true})
+            res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true })
             console.log("user: ", JSON.stringify(user, null, 2))
             console.log("token: ", token)
 
@@ -37,11 +37,55 @@ const createUser = async (req, res) => {
 
         }
     } catch (error) {
-
+        console.log('Error creating user: ', error)
     }
 }
 
+// Login endpoint
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        // find a user by the email above
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        if (user) {
+            const correctPassword = await bcrypt.compare(password, user.password)
+
+            if (correctPassword) {
+                let token = jwt.sign({ id: user.id },
+                    process.env.secretKey, {
+                    expiresIn: 1 * 24 * 60 * 60 * 1000
+                })
+
+                res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true })
+                console.log("user: ", JSON.stringify(user, null, 2))
+                console.log("token: ", token)
+
+                return res.status(201).send({
+                    statusCode: 200,
+                    status: 'success',
+                    message: 'login successful',
+                    data: user
+                })
+            } else {
+                return res.status(401).send({
+                    statusCode: 401,
+                    status: 'success',
+                    message: 'Access denied',
+                })
+            }
+        }
+    } catch (error) {
+        console.log('Error login: ', error)
+    }
+}
 
 module.exports = {
-    createUser
+    createUser,
+    login
 }
